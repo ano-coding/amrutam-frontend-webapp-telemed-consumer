c2a992a97be113200ae48eb4975545f7d720bccb
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-06-13 06:51:20 +0530

    add website favicon, add title, removed unnecessary css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import './App.css'
-
 function App() {
-
   return (
-	<>
-		<h1>Amrutam Consumer App</h1>
+    <>
+      <h1>Amrutam Consumer App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
