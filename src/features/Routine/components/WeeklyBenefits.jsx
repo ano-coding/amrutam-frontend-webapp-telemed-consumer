c5308f5b4a801afd38a598bc9db7fe81470b15a1
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-07-16 09:24:13 +0530

    add confirmation modal for deleting weekly benefits

--- src/features/Routine/components/WeeklyBenefits.jsx
+++ src/features/Routine/components/WeeklyBenefits.jsx
@@ -7,10 +7,11 @@ import HookFormDropDown from "./HookFormDropDown";
 import AddWeeklyBenefits from "./AddWeeklyBenefits";
 import { useLocation, useNavigate } from "react-router-dom";
 import useCreateWeeklyBenefits from "../../../hooks/routines/useCreateWeeklyBenefits";
-import { useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import { UserContext } from "../../../context/UserContext";
 import useUpdateWeeklyBenefit from "../../../hooks/routines/useUpdateWeeklyBenefits";
 import useDeleteWeeklyBenefits from "../../../hooks/routines/useDeleteWeeklyBenefits";
+import ConfirmationModal from "./ConfirmationModal";
 const breadCrumbList = [
   {
     name: "Routines",
@@ -31,6 +32,7 @@ const breadCrumbList = [
 ];
 
 const WeeklyBenefits = () => {
+  const [confirmDeleteModal, setConfirmDeleteModal] = useState(false);
   const navigate = useNavigate();
   const { token } = useContext(UserContext);
   const { createWeeklyBenefitsMutate, createWeeklyBenefitsStatus } =
@@ -38,6 +40,7 @@ const WeeklyBenefits = () => {
   const { updateBenefitMutate, updateBenefitStatus } = useUpdateWeeklyBenefit();
   const { deleteWeeklyBenefitsMutate, deleteWeeklyBenefitsStatus } =
     useDeleteWeeklyBenefits();
+  const isDeleting = deleteWeeklyBenefitsStatus === "pending";
   const { state } = useLocation();
   const totalWeeks = state?.duration?.number;
 
@@ -77,89 +80,140 @@ const WeeklyBenefits = () => {
   };
 
   return (
-    <div className="flex w-full flex-col gap-[37px]">
-      <Breadcrumb list={breadCrumbList} />
-      <div className="flex items-center gap-3 rounded-[15px] bg-[#FFF7E2] px-2 py-4 lg:px-4 lg:py-6">
-        <div className="flex items-center justify-center rounded-full bg-[#EAF2EA] p-1">
-          <BiQuestionSvg className={`size-7 fill-[#3a643b]`} />
+    <Fragment>
+      <div className="flex w-full flex-col gap-[37px]">
+        <Breadcrumb list={breadCrumbList} />
+        <div className="flex items-center gap-3 rounded-[15px] bg-[#FFF7E2] px-2 py-4 lg:px-4 lg:py-6">
+          <div className="flex items-center justify-center rounded-full bg-[#EAF2EA] p-1">
+            <BiQuestionSvg className={`size-7 fill-[#3a643b]`} />
+          </div>
+          <div className="text-[16px] font-light text-[#3A643B]">
+            This weekly benefit will help potential users track their weekly
+            progress while using this routine.
+          </div>
         </div>
-        <div className="text-[16px] font-light text-[#3A643B]">
-          This weekly benefit will help potential users track their weekly
-          progress while using this routine.
-        </div>
-      </div>
-      <ContentBoxLayout title="Add Weekly Benefits">
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full">
-            <div className="mb-2 flex w-full flex-col gap-10 rounded-xl px-5 py-4 lg:pr-16">
-              <div className="flex items-center justify-between text-[18px] font-medium text-black">
-                <span>Enter Weekly Benefits</span>
-                {isEdit && (
-                  <TrashSolidSvg
+        <ContentBoxLayout title="Add Weekly Benefits">
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full">
+              <div className="mb-2 flex w-full flex-col gap-10 rounded-xl px-5 py-4 lg:pr-16">
+                <div className="flex items-center justify-between text-[18px] font-medium text-black">
+                  <span>Enter Weekly Benefits</span>
+                  {isEdit && (
+                    <TrashSolidSvg
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setConfirmDeleteModal(true);
+                      }}
+                      className="size-6 cursor-pointer fill-red-600"
+                    />
+                  )}
+                </div>
+                <div className="flex flex-col gap-0.5">
+                  <HookFormDropDown
+                    disabled={isEdit}
+                    label="Select Week Intervals"
+                    mdWidth="w-[400px]"
+                    list={Array.from({ length: totalWeeks }, (_, i) => i + 1)
+                      .filter((i) => totalWeeks % i === 0)
+                      .map((i) => `${i} week${i > 1 ? "s" : ""}`)}
+                    name="weekInterval"
+                  />
+
+                  <div className="ml-5 text-[12px] text-[#A0A0A0]">
+                    Total Weeks for your “Skin Care Routine” is{" "}
+                    <span className="text-[#3a643b]">{`${totalWeeks} Weeks`}</span>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-10">
+                  {generateWeekObjects(
+                    totalWeeks,
+                    methods.watch("weekInterval"),
+                  ).map((interval) => (
+                    <div
+                      className="flex flex-col gap-3"
+                      key={interval.interval}
+                    >
+                      <div className="text-[16px] font-medium text-black">
+                        {`${interval.weekName} week benefits`}
+                      </div>
+                      <AddWeeklyBenefits
+                        isEdit={isEdit}
+                        weekRange={interval.weekName}
+                      />
+                    </div>
+                  ))}
+                </div>
+                {/* ------------- */}
+                <div className="flex w-full items-center justify-center gap-3 sm:gap-6">
+                  <button
                     onClick={(e) => {
                       e.preventDefault();
-                      deleteWeeklyBenefitsMutate([token, benefits._id], {
-                        onSuccess: () => {
-                          navigate(-1);
-                        },
-                      });
+                      navigate(-1);
                     }}
-                    className="size-6 cursor-pointer fill-red-600"
-                  />
-                )}
-              </div>
-              <div className="flex flex-col gap-0.5">
-                <HookFormDropDown
-                  disabled={isEdit}
-                  label="Select Week Intervals"
-                  mdWidth="w-[400px]"
-                  list={Array.from({ length: totalWeeks }, (_, i) => i + 1)
-                    .filter((i) => totalWeeks % i === 0)
-                    .map((i) => `${i} week${i > 1 ? "s" : ""}`)}
-                  name="weekInterval"
-                />
-
-                <div className="ml-5 text-[12px] text-[#A0A0A0]">
-                  Total Weeks for your “Skin Care Routine” is{" "}
-                  <span className="text-[#3a643b]">{`${totalWeeks} Weeks`}</span>
+                    className={`my-10 box-border rounded-xl border-2 border-[#3A643B] px-5 py-[17px] text-center text-base font-semibold text-[#3A643B] shadow-sm duration-100 hover:bg-[#f4f4f4] active:scale-95 sm:w-[15rem] lg:w-[20rem] xl:w-[23.4rem]`}
+                  >
+                    Cancel
+                  </button>
+                  <button className="my-10 box-border rounded-xl border-2 border-[#3A643B] bg-[#3A643B] px-16 py-[17px] text-center text-base text-white shadow-[0px_4px_14px_rgba(58,_100,_59,_0.25)] duration-100 hover:bg-[#618a61] active:scale-95 md:w-[23.4rem]">
+                    Save Benefits
+                  </button>
                 </div>
               </div>
-              <div className="flex flex-col gap-10">
-                {generateWeekObjects(
-                  totalWeeks,
-                  methods.watch("weekInterval"),
-                ).map((interval) => (
-                  <div className="flex flex-col gap-3" key={interval.interval}>
-                    <div className="text-[16px] font-medium text-black">
-                      {`${interval.weekName} week benefits`}
-                    </div>
-                    <AddWeeklyBenefits
-                      isEdit={isEdit}
-                      weekRange={interval.weekName}
-                    />
-                  </div>
-                ))}
-              </div>
-              {/* ------------- */}
-              <div className="flex w-full items-center justify-center gap-3 sm:gap-6">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    navigate(-1);
-                  }}
-                  className={`my-10 box-border rounded-xl border-2 border-[#3A643B] px-5 py-[17px] text-center text-base font-semibold text-[#3A643B] shadow-sm duration-100 hover:bg-[#f4f4f4] active:scale-95 sm:w-[15rem] lg:w-[20rem] xl:w-[23.4rem]`}
-                >
-                  Cancel
-                </button>
-                <button className="my-10 box-border rounded-xl border-2 border-[#3A643B] bg-[#3A643B] px-16 py-[17px] text-center text-base text-white shadow-[0px_4px_14px_rgba(58,_100,_59,_0.25)] duration-100 hover:bg-[#618a61] active:scale-95 md:w-[23.4rem]">
-                  Save Benefits
-                </button>
-              </div>
+            </form>
+          </FormProvider>
+        </ContentBoxLayout>
+      </div>
+      <ConfirmationModal
+        open={confirmDeleteModal}
+        setOpen={setConfirmDeleteModal}
+        render={() => (
+          <>
+            <h1
+              className={
+                "font-poppins self-start text-2xl font-semibold text-red-700"
+              }
+            >
+              Delete
+            </h1>
+            <p className="font-poppins text-start text-sm font-medium">
+              {`Are you sure you want to delete `}
+              <span className="font-semibold">{name}</span> {`Routine? `}
+              <span className="font-semibold tracking-wide">
+                This cannot be undone.
+              </span>
+            </p>
+            <div className="flex w-full justify-end gap-6">
+              <button
+                disabled={isDeleting}
+                onClick={() => setConfirmDeleteModal(false)}
+                className={`font-poppins font-semibold text-neutral-600 ${
+                  isDeleting ? "opacity-55" : ""
+                }`}
+              >
+                Cancel
+              </button>
+              <button
+                disabled={isDeleting}
+                onClick={(e) => {
+                  e.preventDefault();
+                  deleteWeeklyBenefitsMutate([token, benefits._id], {
+                    onSuccess: () => {
+                      setConfirmDeleteModal(false);
+                      navigate(-1);
+                    },
+                  });
+                }}
+                className={`font-poppins font-semibold text-red-700 ${
+                  isDeleting ? "opacity-55" : ""
+                }`}
+              >
+                Confirm
+              </button>
             </div>
-          </form>
-        </FormProvider>
-      </ContentBoxLayout>
-    </div>
+          </>
+        )}
+      />
+    </Fragment>
   );
 };
 
